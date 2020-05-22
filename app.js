b75f85eed90f8ad173fea6010fae6323fa454958
Kevin Loverde <kloverde@gmail.com>
2020-05-22 12:50:39 -0400

    set to port 80

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('./mysql_config');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
